74818c40e03b35005ca5cf16d08c1fd12494a758
FelipheOsorio <feeh_lepinhoo@hotmail.com>
2023-05-24 01:14:11 -0300

    Melhoria na lógica do app, diminuindo o risco de bugs

--- app/src/main/java/devandroid/felipe/gastoviagem/MainActivity.kt
+++ app/src/main/java/devandroid/felipe/gastoviagem/MainActivity.kt
@@ -3,7 +3,10 @@ package devandroid.felipe.gastoviagem
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_ENTER
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import devandroid.felipe.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,74 +16,74 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enabledButton(false)
 
         val travelExpenses = Viagem()
 
-        binding.editDistance.setOnKeyListener { _, i, event ->
-            if (i == KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
 
+        binding.editDistance.setOnFocusChangeListener { _, _ ->
+            val fieldDistanceFocused = binding.editDistance.isFocused
+
+            if(fieldDistanceFocused) {
+                travelExpenses.distance = capturarData(binding.editDistance, travelExpenses)
+            } else {
                 travelExpenses.distance = binding.editDistance.text.toString()
-
-                if(validateData(travelExpenses)) {
-                    enabledButton(true)
-                } else {
-                    enabledButton(false)
-                }
-
-                true
-
-            } else if(i == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_UP) {
-                enabledButton(false)
-                true
-            } else {
-                false
+                if(validateData(travelExpenses)) enabledButton(true) else enabledButton(false)
             }
         }
 
-        binding.editPrice.setOnKeyListener { _, i, event ->
-            if (i == KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
 
+        binding.editPrice.setOnFocusChangeListener { _, _ ->
+            val fieldPriceFocused = binding.editPrice.isFocused
+
+            if(fieldPriceFocused) {
+                travelExpenses.price = capturarData(binding.editPrice, travelExpenses)
+            } else {
                 travelExpenses.price = binding.editPrice.text.toString()
-
-                if(validateData(travelExpenses)) {
-                    enabledButton(true)
-                } else {
-                    enabledButton(false)
-                }
-
-                true
-
-            } else if(i == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_UP) {
-                enabledButton(false)
-                true
-            } else {
-                false
+                if(validateData(travelExpenses)) enabledButton(true) else enabledButton(false)
             }
         }
 
-        binding.editAutonomy.setOnKeyListener { _, i, event ->
-            if(i == KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
 
+        binding.editAutonomy.setOnFocusChangeListener { _, _ ->
+            val fieldAutonomyFocused = binding.editAutonomy.isFocused
+
+            if(fieldAutonomyFocused) {
+                travelExpenses.autonomy = capturarData(binding.editAutonomy, travelExpenses)
+            } else {
                 travelExpenses.autonomy = binding.editAutonomy.text.toString()
-
-                if(validateData(travelExpenses)) {
-                    enabledButton(true)
-                } else {
-                    enabledButton(false)
-                }
-
-                true
-            } else if(i == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_UP) {
-                enabledButton(false)
-                true
-            } else {
-                false
+                if(validateData(travelExpenses)) enabledButton(true) else enabledButton(false)
             }
         }
+
 
         binding.buttonCalculate.setOnClickListener {
             calculate(travelExpenses)
         }
+
+    }
+
+    private fun capturarData(field: EditText, dataField: Viagem) : String {
+        var valueField = field.text.toString()
+
+        field.setOnKeyListener { _, i, event ->
+            if(i == KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                valueField = field.text.toString()
+                if(validateData(dataField)) enabledButton(true) else enabledButton(false)
+                return@setOnKeyListener true
+
+            } else if(i == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_UP) {
+                valueField = field.text.toString()
+
+                when {
+                    valueField.isBlank() || valueField.isEmpty() -> enabledButton(false)
+                    else -> if(validateData(dataField)) enabledButton(true)
+                }
+            }
+            return@setOnKeyListener false
+        }
+
+        return valueField
 
     }
 
